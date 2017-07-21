b3a2cde49b8403eb38a2538bac7d89ff7993786d
Pieter Hordijk <info@pieterhordijk.com>
2017-07-21 19:59:06 +0200

    Added Command tests

---
+++ tests/Unit/CommandPhpTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncPasswordTests;
+
+use PeeHaa\AsyncPassword\Command;
+use PHPUnit\Framework\TestCase;
+
+class CommandPhpTest extends TestCase
+{
+    private $phpFile;
+
+    private $emptyParameters;
+
+    public function setUp()
+    {
+        if (PHP_SAPI === 'phpdbg') {
+            $this->markTestSkipped('Running using phpdbg.');
+
+            return;
+        }
+
+        $this->phpFile         = realpath(__DIR__ . '/../../bin/password_hash.php');
+        $this->emptyParameters = base64_encode(json_encode([]));
+    }
+
+    public function testGetCommandStringWithoutParameters()
+    {
+        $command = new Command('password_hash.php');
+
+        $expected = sprintf('"%s" "%s" %s', PHP_BINARY, $this->phpFile, $this->emptyParameters);
+
+        $this->assertSame($expected, $command->getCommandString());
+    }
+
+    public function testGetCommandStringWithParameters()
+    {
+        $command = new Command('password_hash.php', ['foo' => 'bar']);
+
+        $expected = sprintf('"%s" "%s" %s', PHP_BINARY, $this->phpFile, base64_encode(json_encode(['foo' => 'bar'])));
+
+        $this->assertSame($expected, $command->getCommandString());
+    }
+
+    public function testGetCommandStringWithParametersEncodesCorrectly()
+    {
+        $command = new Command('password_hash.php', ['foo' => 'bar']);
+
+        $parts = explode(' ', $command->getCommandString());
+
+        $encodedParameters = json_decode(base64_decode(end($parts)), true);
+
+        $this->assertSame(['foo' => 'bar'], $encodedParameters);
+    }
+}
